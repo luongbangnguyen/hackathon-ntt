30fc2103f0cd04c220103f00fcaf0baee8abbea9
Nguyen Luong Bang <luongbangvh@gmail.com>
2016-03-10 05:13:38 +0700

    change properties

--- src/main/java/com/vn/ntt/constant/SystemConstant.java
+++ src/main/java/com/vn/ntt/constant/SystemConstant.java
@@ -4,6 +4,6 @@ package com.vn.ntt.constant;
  * Created by bangnl on 3/9/16.
  */
 public class SystemConstant {
-    public final static String AUTHORIZATION = "com.vn.ntt.authorization-key";
-    public final static String SERVER_GCM = "com.vn.ntt.server-gcm";
+    public final static String AUTHORIZATION = "com.vn.ntt.authorization.key";
+    public final static String SERVER_GCM = "com.vn.ntt.server.gcm";
 }
